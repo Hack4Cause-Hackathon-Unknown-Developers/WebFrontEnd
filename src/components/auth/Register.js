f14e1b0f15d0af918ab80e7850fd33f0cbead8fb
Manvendra Bansal <bansalmanvendra@gmail.com>
2021-05-01 22:22:21 +0530

    changed link

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -78,7 +78,7 @@ const RegisterRetailer = () => {
           headers: {
           'content-type': 'application/json'
         }}
-        const res  = await axios.post("http://e9af1ac45e0a.ngrok.io/signup/",registerData,config)
+        const res  = await axios.post("http://0f13e9d9366d.ngrok.io/signup",registerData,config)
         if(res.data.success === true){
           localStorage.setItem('log', true)
           localStorage.setItem('name', registerData.username)
@@ -99,7 +99,7 @@ const RegisterRetailer = () => {
           headers: {
           'content-type': 'application/json'
         }}
-        const res  = await axios.post("http://e9af1ac45e0a.ngrok.io/login/",signInData,config)
+        const res  = await axios.post("http://0f13e9d9366d.ngrok.io/login",signInData,config)
         console.log(res)
         if(res.data.success === true){
           localStorage.setItem('log', true)
