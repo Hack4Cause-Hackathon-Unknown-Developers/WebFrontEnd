d66f024b702aabdb98f8323e74986a2cbc7ab152
Mohan Kumar K <mk6386223@gmail.com>
2021-05-01 22:27:07 +0530

    conflict cleared

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -78,12 +78,9 @@ const RegisterRetailer = () => {
           headers: {
           'content-type': 'application/json'
         }}
-<<<<<<< HEAD
-        const res  = await axios.post("http://0f13e9d9366d.ngrok.io/signup",registerData,config)
-=======
-        const res  = await axios.post("http://e9af1ac45e0a.ngrok.io/signup/",registerData,config)
 
->>>>>>> 197213dd10de6827c2d0f426086670864a528731
+        const res  = await axios.post("http://0f13e9d9366d.ngrok.io/signup",registerData,config)
+
         if(res.data.success === true){
           localStorage.setItem('log', true)
           localStorage.setItem('name', registerData.username)
